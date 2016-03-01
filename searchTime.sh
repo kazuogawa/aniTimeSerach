ebfb3ec0af9128c1a5f67d427fd5444d35fa31d0
kazuogawa <poppoppan04@gmail.com>
2016-03-02 00:15:18 +0900

    begintimeを抜き出すところまでできた。 残り begintime抜き出した行番号+7くらいの行を抜き出し その行のendtime抜き出し begintimeとendtimeをそれぞれ秒に直す。 引き算
    で終わるはず

--- searchTime.sh
+++ searchTime.sh
@@ -6,11 +6,25 @@ do
 	read ani
 	echo 'ファイル名を入力してください(trace.logならそのままEnter)'
 	read pass
+	# なにも入力せずEnterだったらtrace.logを入れる
 	if [ -z ${pass} ]; then
 		pass="trace.log"
 	fi
+	# aniを含めた1行を抽出
 	aniLine=$(grep $ani "./$pass")
 	echo $aniLine
+	# sidを抽出
 	sid=`echo $aniLine | cut -c 32-64`
 	echo $sid
+	# sidから1家電のlogを抽出
+	oneTellLog=$(grep $sid "./$pass")
+	# もしかしたら別のも引っ掛けるかもしれない。最後に改行を含めたいが書き方がわからない
+	beginTimeLine=$(grep "servlet : Srv_C00001" "./$pass")
+	echo $beginTimeLine
+	# 始まりの時間
+	beginTimeString=`echo $beginTimeLine | cut -c 12-24`
+	echo $beginTimeString
+	# C01の開始行
+	beginTimeLineNum=`echo $(grep -n "$beginTimeLine" "./$pass") | cut -d ':' -f1`
+	echo $beginTimeLineNum
 done
